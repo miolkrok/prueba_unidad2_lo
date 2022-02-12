d4480c215a2ff60c810fbf5a7637c147e959236c
Luis Ortiz <luchoaam97@gmail.com>
2022-02-11 19:22:09 -0500

    primer commit prueba unidad 2

---
+++ src/main/java/ec/edu/uce/PruebaUnidad2LoApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.uce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaUnidad2LoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaUnidad2LoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/uce/PruebaUnidad2LoApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.uce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaUnidad2LoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
